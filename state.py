e6976204dc2a4b44b7015f161e609c54a0b4c256
Revprm <revy.pramana@gmail.com>
2023-11-19 13:25:44 +0700

    fix: codeflow

--- state.py
+++ state.py
@@ -1,21 +1,21 @@
 from map import Map
 
 class State:
-    def __init__(self, person: tuple, treasure=[]) -> None:
+    def __init__(self, person: tuple, lever=[]) -> None:
         self.person = person
-        self.treasure = treasure
+        self.lever = lever
 
     def __eq__(self, other):
-        return self.treasure == other.treasure and self.person == other.person
+        return self.lever == other.lever and self.person == other.person
     
     def __str__(self):
-        return '"Person at: ' + str(self.person) + 'treasure at: ' + str(self.treasure) + '"'
+        return '"Person at: ' + str(self.person) + 'lever at: ' + str(self.lever) + '"'
     
     def __repr__(self):
         return self.__str__()
     
     def __hash__(self):
         h = hash(self.person)
-        for i in self.treasure:
+        for i in self.lever:
             h += hash(i)
         return h
