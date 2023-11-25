f197255c72ddec277b36d8172bf3643bbd509fe1
Revprm <revy.pramana@gmail.com>
2023-11-25 17:16:09 +0700

    refactor: Deleted error files

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-or i in range(len(result)):
-    #     p2 = result[i].person
-    #     print(directions[(p2[0]-p1[0], p2[1]-p1[1])], end='')
-    #     # print(p2, p1, (p2[0]-p1[0], p2[1]-p1[1]))
-    #     p1 = result[i].person
